2eca99fea4c682e110fdb40cd4d2eb0e23559026
Prasanth K S <ksprasanthksp10@gmail.com>
2025-01-24 23:16:35 +0530

    Vercel Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,42 @@
-
-import Home from "./components/FunctionalComponents/Home"
-import About from "./components/FunctionalComponents/About"
-import Gallery from "./components/FunctionalComponents/Gallery"
-import Contact from "./components/FunctionalComponents/Contact"
-import Navbar from "./components/FunctionalComponents/Navbar"
-import Signup from "./components/FunctionalComponents/Signup"
-import Login from "./components/FunctionalComponents/Login"
-import UseState from "./components/FunctionalComponents/Hooks/UseState"
-import UseEffect from "./components/FunctionalComponents/Hooks/UseEffect"
-import UseEffectAPI from "./components/FunctionalComponents/Hooks/UseEffectAPI"
-import UseEffectImageAPI from "./components/FunctionalComponents/Hooks/UseEffectImageAPI"
-import UseRef from "./components/FunctionalComponents/Hooks/UseRef"
-import UseMemo from "./components/FunctionalComponents/Hooks/UseMemo"
-import UseCallback from "./components/FunctionalComponents/Hooks/UseCallback"
-import UseReducer from "./components/FunctionalComponents/Hooks/UseReducer"
-import ReactLifecycleMethods from "./components/classComponents/ReactLifecycleMethods"
-import UseContext from "./components/FunctionalComponents/Hooks/UseContext"
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import "./App.css";
-import { useState } from "react";
-import Memo from "./components/FunctionalComponents/Memoization/Memo"
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/FunctionalComponents/Home';
+import About from './components/FunctionalComponents/About';
+import Gallery from './components/FunctionalComponents/Gallery';
+import Contact from './components/FunctionalComponents/Contact';
+import Navbar from './components/FunctionalComponents/Navbar';
+import Signup from './components/FunctionalComponents/Signup';
+import Login from './components/FunctionalComponents/Login';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const handleLogin = () => {
-    setIsAuthenticated(true);
+  const handleLogin = () => setIsAuthenticated(true);
+  const handleLogout = () => setIsAuthenticated(false);
 
-  };
-
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-
-  };
-
-  return (<div>
-    <BrowserRouter>
-      {!isAuthenticated ? (
-        <>
+  return (
+    <div>
+      <BrowserRouter>
+        {!isAuthenticated ? (
           <Routes>
             <Route path="/" element={<Signup onLogin={handleLogin} />} />
             <Route path="/Login" element={<Login onLogin={handleLogin} />} />
           </Routes>
-        </>
-      ) : (
-        <>
-          <Navbar />
-          <Routes>
-            <Route path='/Home' element={<Home />}></Route>
-            <Route path='/About' element={<About />}></Route>
-            <Route path="/Gallery" element={<Gallery image="SECE logo" page="Gallery" />}></Route>
-            <Route path="/Contact" element={<Contact />}></Route>
-            <Route path="/UseState" element={<UseState />}></Route>
-            <Route path="/UseEffect" element={<UseEffect />}></Route>
-            <Route path="UseEffectAPI" element={<UseEffectAPI/>}></Route>
-            <Route path="UseEffectImageAPI" element={<UseEffectImageAPI/>}></Route>
-            <Route path="/UseRef" element={<UseRef/>}></Route>
-            <Route path="/UseMemo" element={<UseMemo/>}></Route>
-            <Route path="/UseCallback" element={<UseCallback/>}></Route>
-            <Route path="/UseReducer" element={<UseReducer/>}></Route>
-            <Route path="/ReactLifecycleMethods" element={<ReactLifecycleMethods/>}></Route>
-            <Route path="/UseContext" element={<UseContext/>}></Route>
-            <Route path="/Memo" element={<Memo/>}></Route>
-            
-        
-          </Routes>
-        </>
-      )}
+        ) : (
+          <>
+            <Navbar onLogout={handleLogout} />
+            <Routes>
+              <Route path="/Home" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Gallery" element={<Gallery />} />
+              <Route path="/Contact" element={<Contact />} />
+            </Routes>
+          </>
+        )}
+      </BrowserRouter>
+    </div>
+  );
+}
 
-    </BrowserRouter>
-  </div >
-  )
-};
-
-export default App;
+export default App;
